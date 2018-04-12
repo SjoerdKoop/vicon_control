5b068422b6030c079637b2cd4f47fe904f58f0f5
Sjoerd Koopmans <s.j.m.koopmans@student.tue.nl>
2018-04-12 12:51:40 +0200

    Merged robot control project into a single make project

---
+++ robot_control/src/read_encoders/src/read_encoders.cpp
@@ -0,0 +1,26 @@
+// PRU
+// Always include stdint.h first 
+#include <stdint.h>						// Type definitions for PRU headers
+#include "am335x/pru_cfg.h"				// CT_CFG
+
+// Resource table
+// A resource table is required for the PRU to run
+#include "resource_table_empty.h"		// Empty resource table
+
+// Read encoders
+#include "encoder.h"
+
+// Main function
+int main() {
+	// Enable OCP
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	// Create encoder with pins 0 and 1 and link memory location 0 to this encoder
+	Encoder* encoder0 = new Encoder(0, 1, 0);
+
+	// Loop infinitely
+	while(1) {
+		// Update encoder
+		encoder0->update();
+	}
+}
